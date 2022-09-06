40562d8dbf0e298adcba47ab64cc655023561af0
VlzZ <vlzz1221@gmail.com>
2022-09-06 22:21:10 +0800

    Update removeImages.js

--- removeImages.js
+++ removeImages.js
@@ -1,8 +1,9 @@
 var baiduNodes = document.querySelectorAll(".slide-baidu");
 var i;
 for (i = 0; i < baiduNodes.length; i++) {
-    var _parentElement = baiduNodes[i].parentNode;
-    if(_parentElement){
-        _parentElement.removeChild(baiduNodes[i]); 
-    }
+    baiduNodes[i].style.backgroundColor = "red";
+//     var _parentElement = baiduNodes[i].parentNode;
+//     if(_parentElement){
+//         _parentElement.removeChild(baiduNodes[i]); 
+//     }
 }
